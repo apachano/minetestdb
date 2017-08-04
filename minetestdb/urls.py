c6cd96c9d29cd37179ac0d02c5231997bc7a6037
Austin <pachano.austin@gmail.com>
2017-08-04 02:41:55 -0700

    Added Sign up

--- minetestdb/urls.py
+++ minetestdb/urls.py
@@ -20,11 +20,12 @@ from . import views
 
 app_name = 'minetestdb'
 urlpatterns = [
-    url(r'^login/', auth_views.login, name='login'),
-    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^mods/', include('mods.urls')),
     url(r'^servers/', include('servers.urls')),
     url(r'^users/', include('users.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 ]
