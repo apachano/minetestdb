03e3e8c2238db892e93d9cfa46c6afc9cdb5d221
Austin <pachano.austin@gmail.com>
2017-08-14 19:48:06 -0700

    Created page for editing profiles
    Page checks if user is logged in

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ app_name = 'users'
 urlpatterns = [
     url(r'^/$', views.index, name='index'),
     url(r'^/(?P<username>\w+)/$', views.detail, name='detail'),
-    url(r'^/(?P<username>\w+)/profile/$', views.profile, name='profile'),
+    url(r'^-profile/$', views.profile, name='profile'),
     url(r'^-logout/', views.logout_view, name='logout'),
     url(r'^-login/', views.login_view, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,15 +3,25 @@ from django.contrib.auth import logout
 from django.contrib.auth.models import User
 
 
+# Lists registered users
 def index(request):
     user_list = User.objects.all()
     context = {'user_list': user_list}
     return render(request, 'users/index.html', context)
 
 
+# Displays information on a user
 def detail(request, username):
     user = get_object_or_404(User, username=username)
     return render(request, 'users/detail.html', {'user': user})
+
+
+# Allows user to edit their profile
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+    else:
+        return render(request, 'users/profile.html')
 
 
 def logout_view(self, request):
@@ -30,7 +40,3 @@ def login_view(request):
     else:
         redirect("/")
 
-
-def profile(request):
-    user = get_object_or_404(User, username=username)
-    return render(request, 'users/profile.html', {'user': user})
