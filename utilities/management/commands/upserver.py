b968b4c0a18afdd94efd852b6affe3f80a3a8a47
M3TIOR <jtimmerman32@gmail.com>
2018-01-10 22:00:09 -0800

    fixed discrepency in verbosity levels

--- utilities/management/commands/upserver.py
+++ utilities/management/commands/upserver.py
@@ -15,8 +15,17 @@ class Command(BaseCommand):
 		generates test data using the unittests and then runs runserver.
 	"""
 
+	def _get_action_by_dest(self, parser, dest):
+		for action in parser._get_optional_actions():
+			if action.dest == dest:
+				return action
+
 	def add_arguments(self, parser):
 		# Uses argparse.ArgumentParser()
+
+		# Override default verbosity value
+		self._get_action_by_dest(parser, "verbosity").default = -1
+
 		parser.add_argument(
 			"-V", "--global-verbosity",
 			help="""
@@ -24,35 +33,43 @@ class Command(BaseCommand):
 				upserver uses, including the verbosity level for this command
 				itself.
 			""",
-			type=int, default=1
+			choices=[0,1,2,3],
+			type=int, default=0
 		)
 		parser.add_argument(
 			"--makemigrations-verbosity",
 			help="sets the verbosity level for makemigrations",
-			type=int, default=0
+			choices=[0,1,2,3],
+			type=int, default=1
 		)
 		parser.add_argument(
 			"--migrate-verbosity",
 			help="sets the verbosity level for migrate",
-			type=int, default=0
+			choices=[0,1,2,3],
+			type=int, default=-1
 		)
 		parser.add_argument(
 			"--runserver-verbosity",
 			help="sets the verbosity level for runserver",
-			type=int, default=0
+			choices=[0,1,2,3],
+			type=int, default=-1
 		)
 
 	def handle(self, *args, **options):
+		verbosity = (options["verbosity"]
+			if options['verbosity'] > -1
+			else options['global_verbosity']
+		)
 		makemigrations_verbosity = (options['makemigrations_verbosity']
-			if options['makemigrations_verbosity']
+			if options['makemigrations_verbosity'] > -1
 			else options['global_verbosity']
 		)
 		migrate_verbosity = (options['migrate_verbosity']
-			if options['migrate_verbosity']
+			if options['migrate_verbosity'] > -1
 			else options['global_verbosity']
 		)
 		runserver_verbosity = (options['runserver_verbosity']
-			if options['runserver_verbosity']
+			if options['runserver_verbosity'] > -1
 			else options['global_verbosity']
 		)
 
