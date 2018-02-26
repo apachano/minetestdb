9ef8c112acd72c61fe311c8f57fd62fb75954774
Austin <pachano.austin@gmail.com>
2018-02-25 17:15:19 -0800

    Fixed User sign up error

--- minetestdb/views.py
+++ minetestdb/views.py
@@ -21,7 +21,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
