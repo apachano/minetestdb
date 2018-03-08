9dd60de9d90da01558203ee6938c00aec8602a75
M3TIOR <jtimmerman32@gmail.com>
2018-03-08 10:57:24 -0800

    misc testing

--- debug-5.7-database.sql
+++ debug-5.7-database.sql
@@ -2,8 +2,8 @@
 
 DROP DATABASE IF EXISTS `mtcd`;
 CREATE DATABASE `mtcd`
-    DEFAULT CHARACTER SET utf8
-    DEFAULT COLLATE utf8_general_ci;
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
 
 USE `mtcd`;
 GRANT ALL PRIVILEGES ON mtcd.* TO 'site'@'localhost' IDENTIFIED BY 'agfh1085'
@@ -16,8 +16,8 @@ FLUSH PRIVILEGES;
 #Makes a debug database without a lot of setup.
 DROP DATABASE IF EXISTS `mtcd_debug`;
 CREATE DATABASE `mtcd_debug`
-    DEFAULT CHARACTER SET utf8
-    DEFAULT COLLATE utf8_general_ci;
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
 
 USE `mtcd_debug`;
 GRANT ALL PRIVILEGES ON mtcd_debug.* TO 'site'@'localhost' IDENTIFIED BY 'agfh1085'
