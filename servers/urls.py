71caa87825c56ed171a34957a3d6134c4dd54588
M3TIOR <jtimmerman32@gmail.com>
2018-03-14 10:40:45 -0700

    fixes issue #10: Spaces in mod/server name url
    the issue was caused by improper regex of values.
    I'm assuming what you had in url parser to begin with was something generic
    because it matched all alphanumeric variants of the server name rather than all.
    I left a note in the code for future refference

--- servers/urls.py
+++ servers/urls.py
@@ -5,7 +5,16 @@ from . import views
 app_name = 'servers'
 urlpatterns = [
     url(r'^/$', views.index, name='index'),
-    url(r'^/(?P<name>\w+)/$', views.detail, name='detail'),
+
+    # NOTE:
+    # r'^/(?P<name>\w+)/detail' matches all alphanumeric, no whitespace chars
+    # the current implementation matches all characters
+    #
+    # IF YOU EVER HAVE THIS PROBLEM AGAIN RESEARCH YOUR REGEX:
+    #	https://docs.python.org/3/howto/regex.html
+    #	https://docs.python.org/3/library/re.html
+    #
+    url(r'^/(?P<name>.(?s)+)/detail', views.detail, name='detail'),
     url(r'^-new/$', views.new, name='new'),
-    url(r'^/(?P<name>\w+)/edit', views.edit, name='edit'),
+    url(r'^/(?P<name>.(?s)+)/edit', views.edit, name='edit'),
 ]
